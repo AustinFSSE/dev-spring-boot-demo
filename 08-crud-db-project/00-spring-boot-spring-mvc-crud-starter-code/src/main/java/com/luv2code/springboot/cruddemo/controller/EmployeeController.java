8bab2e3773b12393c26cf434b5d6dd276fec3c1e
Austin Martin <alm2016.in@icloud.com>
2025-03-23 16:35:28 -0400

    13th commit from MacOS

--- 08-crud-db-project/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ 08-crud-db-project/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -4,8 +4,7 @@ import org.springframework.ui.Model;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +23,33 @@ public class EmployeeController {
     public String listEmployees(Model model) {
         List <Employee> employees = employeeService.findAll();
         model.addAttribute("employees", employees);
-        return "list-employees";
+        return "employees/list-employees";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+    @GetMapping("showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id) {
+        employeeService.deleteById(id);
+        return "redirect:/employees/list";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        System.out.println("Employee saved successfully -> " + employee);
+        return "redirect:/employees/list";
+    }
 
 }
