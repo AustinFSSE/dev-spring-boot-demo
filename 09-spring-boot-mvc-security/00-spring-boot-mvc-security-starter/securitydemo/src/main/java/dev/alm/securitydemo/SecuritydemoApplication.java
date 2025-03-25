8e1415d67aca215ada3139681fcf172c0e7aaf32
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2025-03-25 12:52:55 -0400

    commit from windows

---
+++ 09-spring-boot-mvc-security/00-spring-boot-mvc-security-starter/securitydemo/src/main/java/dev/alm/securitydemo/SecuritydemoApplication.java
@@ -0,0 +1,13 @@
+package dev.alm.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritydemoApplication.class, args);
+	}
+
+}
